bf546aecd0df5e3ae9f1d39ef0427e5dd84304b1
Omima <omima.ibrahim93@gmail.com>
2021-02-19 15:03:32 +0100

    add user retrieve unit test

--- QapitalTaskTests/ActivityInteractorTests.swift
+++ QapitalTaskTests/ActivityInteractorTests.swift
@@ -20,6 +20,8 @@ class ActivityInteractorTests: QuickSpec {
         
         // MARK:- Test data
         let oldest =  "2020-10-08T00:00:00+00:00"
+        let userId = 1
+        let user = User(id: 1, name: "Mikael", avatar: "http://qapital-ios-testtask.herokuapp.com/avatars/mikael.jpg")
         
         // MARK:- Testing ActivityInteractor
         describe("testing ActivityInteractor") {
@@ -29,31 +31,29 @@ class ActivityInteractorTests: QuickSpec {
             }
             
             context("Loading data Successfully"){
-
+                
                 context("After getting first request") {
-                    
                     it("shouldn't have next page grater than oldest date") {
-                       
+                        
                         let dateString = "2020-10-01T00:00:00+00:00"
                         let dateFormatter = DateFormatter()
                         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-
                         let date = dateFormatter.date(from:dateString)!
-
                         expect(interactor.hasNextPage(with: date, oldestDate: oldest)).to(beFalse())
                     }
-                    
                 }
-
-                
+                context(" After getting Actvities request") {
+                    beforeEach {
+                        interactor.save(users: [user])
+                    }
+                    
+                    it("should return user if exist") {
+                        expect( interactor.getUserIfExist(with: userId)).to(equal(user))
+                    }
+                }
             }
-            
         }
-        
-        
-        
     }
-    
 }
 
 
